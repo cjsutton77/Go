3423dc16913b3498526a11ddfaf23b32a642ab99
Christian Sutton <christian.sutton@gmail.com>
2020-06-08 21:40:59 -0500

    mm

---
+++ mars.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// main is where it all starts
+
+func main() {
+	var age = 43 * 365 / 687
+	var weight = 215 * 0.3783
+	fmt.Printf("\n\nMy weight on the surface of Mars is: %v lbs\n", weight)
+	//fmt.Println()
+	fmt.Printf("My age on Mars would be: %v years old", age)
+}
